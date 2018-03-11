a4bafc9c0c81dedce04f00dbcbecb4948891afaf
Nedim Becirovic <nedimbe@maestralsolutions.com>
2018-03-11 11:48:41 +0100

    Add basic form with one input field

--- suggest.php
+++ suggest.php
@@ -5,7 +5,14 @@ include("inc/header.php");
 ?>
 
 <div class="section page">
+  <div class="wrapper">
     <h1>Suggest a Media Item</h1>
+    <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+
+    <form action="" method="post">
+      <label for="name">Name</label><input type="text" id="name" name="name">
+    </form>
+  </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
