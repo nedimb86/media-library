daf3e2406563df75fc205f0f3a7b0b5f96aa92e2
Nedim Becirovic <nedimbe@maestralsolutions.com>
2018-03-11 11:53:23 +0100

    Additional fields

--- suggest.php
+++ suggest.php
@@ -10,7 +10,21 @@ include("inc/header.php");
     <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
 
     <form action="" method="post">
-      <label for="name">Name</label><input type="text" id="name" name="name">
+      <table>
+        <tr>
+          <th><label for="name">Name</label></th>
+          <td><input type="text" id="name" name="name"></td>
+        </tr>
+        <tr>
+          <th><label for="email">Email</label></th>
+          <td><input type="text" id="email" name="email"></td>
+        </tr>
+        <tr>
+          <th><label for="details">Suggest</label></th>
+          <td><textarea  id="details" name="details"></textarea></td>
+        </tr>
+      </table>
+      <input type="submit" value="Send">
     </form>
   </div>
 </div>
