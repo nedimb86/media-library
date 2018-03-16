511314fc96675d5ce4a6187f767bdb8dfccd2b51
Nedim Becirovic <nedimbe@maestralsolutions.com>
2018-03-17 00:05:51 +0100

    PDO set attribute

--- inc/connection.php
+++ inc/connection.php
@@ -1,8 +1,10 @@
 <?php
 try {
     $db = new PDO('sqlite:' . __DIR__ . '\database.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
-    echo 'Unable to connect to database';
+    echo 'Unable to connect to database: ';
+    echo $e->getMessage();
     exit;
 }
 echo 'Connected to database';
