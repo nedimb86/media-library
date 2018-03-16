e059ae8a73130cb7c8f036739f6b471b5bd73d9c
Nedim Becirovic <nedimbe@maestralsolutions.com>
2018-03-16 23:54:55 +0100

    Add try -> catch block when connection to database

--- inc/connection.php
+++ inc/connection.php
@@ -1,3 +1,8 @@
 <?php
-
- $db = new PDO('sqlite:'.__DIR__.'\database.db');
+try {
+    $db = new PDO('sqlite:' . __DIR__ . '\database.db');
+} catch (Exception $e) {
+    echo 'Unable to connect to database';
+    exit;
+}
+echo 'Connected to database';
